f7a38731bcad9663bba46d72247a9e1386715e31
ken-sevenseas <inakanookaki@icloud.com>
2023-04-26 01:55:59 +0900

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "05nervier_outfit"
 
     workspaces {
-      name = "learn-terraform-github-actions"
+      name = "gh-actions-demo"
     }
   }
 }
